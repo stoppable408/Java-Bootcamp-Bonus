7c54c29a5e674e72bbec1eec3e5422abc083ab40
Lennon Turner <Lennonturner92@gmail.com>
2016-03-28 17:07:27 -0400

    Updates Bonus01 with correct error handling

--- Bonus01.java
+++ Bonus01.java
@@ -1,29 +1,7 @@
 import java.util.Scanner;
 
 public class Bonus01 {
-	/*
-	 * Lennon Turner Java Bootcamp Bonus Lab 01.
-	 * 
-	 * Creates a program that converts a number grade to it's appropriate letter
-	 * grade. program also contains a do-while loop that prompts the user to
-	 * continue if they type the letter "Y" when prompted.
-	 *
-	 * copies grading rubric from the following source:
-	 * http://www.colostate.edu/Dept/EdLicense/StudentTeaching/grade_scale.pdf
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 */
 
-	// Function gradeCalc takes as input an integer representing a numerical
-	// grade, and then proceeds to go through multiple if-else statements to
-	// check for the appropriate letter grade.
 	public static String gradeCalc(int grade) {
 		if (grade > 97) {
 			return "A+";
@@ -52,12 +30,15 @@ public class Bonus01 {
 		} else if ((grade < 60) && (grade >= 0)) {
 			return "F";
 		}
-		// if a number less than 0 is entered, the program will tell the user
-		// that the input is invalid, and recall the function with a new input.
 		else {
 			Scanner input = new Scanner(System.in);
-			System.out.print("Invalid entry. Please enter another value: ");
-			int newGrade = input.nextInt();
+			int newGrade;
+			do {
+			
+				System.out.print("Invalid entry. Please enter another value: ");
+				newGrade = input.nextInt();
+			
+			} while (grade < 0);
 			input.close();
 			return gradeCalc(newGrade);
 		}
@@ -65,22 +46,22 @@ public class Bonus01 {
 	}
 
 	public static void main(String[] args) {
-		// sets up the input system.
+		
 		Scanner grade = new Scanner(System.in);
 		Scanner answer = new Scanner(System.in);
-		char newAnswer;
+		String newAnswer;
 
 		System.out.println("Welcome to the Letter Grade Converter");
-		// do while loop containing the conversion, and continuing if the user
-		// types "Y" when prompted.
+		
+		
 		do {
 			System.out.print("\nEnter a numerical grade: ");
 			int newGrade = grade.nextInt();
 			System.out.println("Letter Grade: " + gradeCalc(newGrade));
-			System.out.print("\n" + "Continue? (Y/N): ");
-			newAnswer = answer.next(".").charAt(0);
+			System.out.print("\n" + "Continue? (y/n): ");
+			newAnswer = answer.next();
 
-		} while ((Character.toUpperCase(newAnswer)) == 'Y');
+		} while (newAnswer.equalsIgnoreCase("Y"));
 		grade.close();
 		answer.close();
 
