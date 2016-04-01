b606534d1f97d92806929ec93db682f44d058fe8
Lennon Turner <Lennonturner92@gmail.com>
2016-04-01 10:33:05 -0400

    Adds Bonus02 Folder

---
+++ Bonus02/Karaoke/src/com/teamtreehouse/model/Karaoke.java
@@ -0,0 +1,13 @@
+package com.teamtreehouse.model;
+import com.teamtreehouse.KaraokeMachine;
+import com.teamtreehouse.model.Song;
+import com.teamtreehouse.model.SongBook;
+
+public class Karaoke {
+  
+  public static void main(String[] args) {
+    SongBook songBook = new SongBook();
+    KaraokeMachine machine = new KaraokeMachine(songBook);
+    machine.run();
+  }
+}
